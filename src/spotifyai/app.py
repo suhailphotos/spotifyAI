b28aa9627a4560a803f2e6afa9485d53a2138578
Suhail <suhailece@gmail.com>
2024-12-03 05:13:52 +0000

    update script

--- src/spotifyai/app.py
+++ src/spotifyai/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,8 +8,8 @@ def home():
 
 @app.route("/hello", methods=["POST"])
 def hello():
-    # Return the "Hello, World!" message when the button is clicked
-    return render_template("index.html", message="Hello, World!")
+    # Return the "Hello, World!" message as a JSON response
+    return jsonify({"message": "Hello, mummy!"})
 
 if __name__ == "__main__":
     app.run(host="::", port=5000, debug=True)  # Bind to IPv6 and enable debug mode
